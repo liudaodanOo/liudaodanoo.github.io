bb8993db711c2caddf0dfe1e6faa422f910515fa
liuxiaofei <liuxfb@digiwin.com>
2023-12-08 13:56:01 +0800

    build: 修改build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,6 @@ images_path=images
 images_cp_path=.vitepress/dist
 dist_path=dist
 
-# 删除根目录下的dist
-if [ -d "$dist_path" ]; then
-  rm -rf "$dist_path"
-fi
-
 # 生成静态文件
 yarn run docs:build
 
@@ -20,13 +15,13 @@ yarn run docs:build
 cp  -r "$images_path" "$images_cp_path"
 
 # 移动dist
-mv .vitepress/dist dist
+mv -f .vitepress/dist dist
 
 # 切换分支
 git checkout gh-pages
 
 # 将dist目录下的文件移到上一层
-mv dist/* .
+mv -f dist/* .
 
 # 加至暂存区并提交
 git add .
