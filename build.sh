6921549e8f27e6657ecc5226193e85a3f0abd76b
liuxiaofei <liuxfb@digiwin.com>
2023-12-08 15:25:40 +0800

    build: 修改build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,12 @@ images_path=images
 bundle_path=.vitepress/dist
 dist_path=dist
 
-# 生成静态文件
+# 开始打包
+echo -e "\033[31m>>>>> 开始打包"
 yarn run docs:build
 
 # 删除根目录下的dist
+echo -e ">>>>> 删除根目录下的dist"
 if [ -d "$dist_path" ]; then
   rm -rf "$dist_path"
 fi
@@ -20,30 +22,38 @@ fi
 cp  -r "$images_path" "$bundle_path"
 
 # 移动dist
+echo -e ">>>>> 移动dist"
 mv -f "$bundle_path" "$dist_path"
 
 # 切换分支
+echo -e ">>>>> 切换分支"
 git checkout gh-pages
 
 # 将.git .gitignore node_modules移动到dist
+echo -e ">>>>> 将.git .gitignore node_modules移动到dist"
 mv .git "$dist_path"
 mv .gitignore "$dist_path"
 mv node_modules "$dist_path"
 
 # 将dist移动到上层目录
+echo -e ">>>>> 将dist移动到上层目录"
 mv "$dist_path" ../"$dist_path"
 
 # 删除当前目录所有文件
+echo -e ">>>>> 删除当前目录所有文件"
 rm -rf  *
 
 # 将dist目录下的文件移到当前目录
+echo -e ">>>>> 将dist目录下的文件移到当前目录"
 mv ../"$dist_path" "$dist_path"
 mv -f "$dist_path"/* .
 
 # 删除dist目录
+echo -e ">>>>> 删除dist目录"
 rm  -rf "$dist_path"
 
 # 加至暂存区并提交
+echo -e ">>>>> 加至暂存区并提交"
 git add .
 git commit -m "build: 📦打包"
 git push origin
