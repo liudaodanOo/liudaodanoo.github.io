15d40a27f3a80dc88e49b4bd2190009be982fccc
liuxiaofei <liuxfb@digiwin.com>
2023-12-08 11:39:20 +0800

    build: 修改build

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
+#!/bin/bash
+
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
 yarn run docs:build
 
+# 移动dist
+mv .vitepress/dist dist
+
 # 切换分支
-git stash
 git checkout gh-pages
-git stash pop
+
+# 将dist目录下的文件移到上一层
+mv * ..
 
 # 加至暂存区并提交
 git add .
